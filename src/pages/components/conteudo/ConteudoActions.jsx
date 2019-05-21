8913b1b2acc8d846aa1bafa7e81bce72d25ceba0
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2019-05-20 22:22:18 -0300

    ConteudoActions em Grid

--- src/pages/components/conteudo/ConteudoActions.jsx
+++ src/pages/components/conteudo/ConteudoActions.jsx
@@ -1,51 +1,75 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import CardActions from '@material-ui/core/CardActions';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import LikeIcon from '@material-ui/icons/ThumbUp';
-import ShareIcon from '@material-ui/icons/Share';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import CardActions from "@material-ui/core/CardActions";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import LikeIcon from "@material-ui/icons/ThumbUp";
+import DislikeIcon from "@material-ui/icons/ThumbDown";
+import ShareIcon from "@material-ui/icons/Share";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import IconButton from '@material-ui/core/IconButton';
-import classnames from 'classnames';
-import * as Map from '../../../Maps';
-import styles from './style.js'
+import IconButton from "@material-ui/core/IconButton";
+import classnames from "classnames";
+import * as Map from "../../../Maps";
+import styles from "./style.js";
 
-const ConteudoActions = (props) => {
-    const { classes, postagemAtual } = props;
+import Grid from "@material-ui/core/Grid";
 
-    return (
-        <CardActions className={classes.actions} disableActionSpacing>
-            <IconButton aria-label="Cultyr" className={classes.botao} onClick={() => props.passarPost()}>
-                <LikeIcon />
-            </IconButton>
-            <IconButton aria-label="Compartilhar" className={classes.botaoShare}>
-                <ShareIcon />
-            </IconButton>
-            {
-                postagemAtual.tipoMidia === "Texto" &&
-                <IconButton
-                    className={classnames(classes.expand, {
-                        [classes.expandOpen]: props.expanded,
-                    })}
-                    onClick={() => props.expandirPostagem()}
-                    aria-expanded={props.expanded}
-                    aria-label="Quero ler mais"
-                >
-                    <ExpandMoreIcon />
-                </IconButton>
-            }
-        </CardActions>
-    );
-}
+const ConteudoActions = props => {
+  const { classes, postagemAtual } = props;
+
+  return (
+    <CardActions className={classes.actions} disableActionSpacing>
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+      >
+        <IconButton
+          aria-label="Dislike"
+          className={classes.botaoDislike}
+          onClick={() => props.passarPost()}
+        >
+          <DislikeIcon />
+        </IconButton>
+
+        <IconButton aria-label="Compartilhar" className={classes.botaoShare}>
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          aria-label="Cultyr"
+          className={classes.botao}
+          onClick={() => props.passarPost()}
+        >
+          <LikeIcon />
+        </IconButton>
+        {postagemAtual.tipoMidia === "Texto" && (
+          <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: props.expanded
+            })}
+            onClick={() => props.expandirPostagem()}
+            aria-expanded={props.expanded}
+            aria-label="Quero ler mais"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        )}
+      </Grid>
+    </CardActions>
+  );
+};
 
 const mapStateToProps = store => {
-    const postagemAtual = store.posts.postagemAtual;
-    return {
-        postagemAtual
-    };
+  const postagemAtual = store.posts.postagemAtual;
+  return {
+    postagemAtual
+  };
 };
 
 export default withRouter(
-    connect(mapStateToProps, Map.mapDispatchToProps)(withStyles(styles)(ConteudoActions))
+  connect(
+    mapStateToProps,
+    Map.mapDispatchToProps
+  )(withStyles(styles)(ConteudoActions))
 );
--- src/pages/components/conteudo/style.js
+++ src/pages/components/conteudo/style.js
@@ -1,67 +1,66 @@
-
 export default theme => ({
   card: {
     maxWidth: 900,
-    backgroundColor: '#1e1e1e',
-    width: '100%',
-    minHeight: '40vh',
+    backgroundColor: "#1e1e1e",
+    width: "100%",
+    minHeight: "40vh"
   },
   estiloTexto: {
-    color: '#ECF2EC',
+    color: "#ECF2EC"
   },
   botao: {
-    color: '#ff9703',
+    color: "#ff9703"
   },
   botaoShare: {
-    color: '#ff9703',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: '32%',
+    color: "#ff9703"
+  },
+  botaoDislike: {
+    color: "#ff9703"
   },
   actions: {
-    alignSelf : "flex-end"
+    alignSelf: "flex-end"
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    color: '#FF8C00',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    color: "#FF8C00",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)"
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   details: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
   },
   content: {
-    flex: '1 0 auto',
+    flex: "1 0 auto"
   },
   cover: {
-    width: 151,
+    width: 151
   },
   controls: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     paddingLeft: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit
   },
   playIcon: {
     height: 38,
-    width: 38,
+    width: 38
   },
   estiloSubTitulo: {
-    color: '#FFDEB5'
+    color: "#FFDEB5"
   },
   avatar: {
-    backgroundColor: '#FF8C00',
+    backgroundColor: "#FF8C00"
   }
 });
