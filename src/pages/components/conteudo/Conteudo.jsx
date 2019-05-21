c04e619f9d48a63b71ffb02097c93c0f0e4d7f5a
Gustavo Henrique da Silva Batista <gustavo.batista@supera.com.br>
2019-05-21 13:44:22 -0300

    Refatorando conteúdo

--- src/pages/components/conteudo/Conteudo.jsx
+++ src/pages/components/conteudo/Conteudo.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Collapse from '@material-ui/core/Collapse';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Collapse from "@material-ui/core/Collapse";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import ConteudoHeader from './ConteudoHeader'
-import ConteudoActions from './ConteudoActions'
-import ConteudoMidia from './ConteudoMidia'
-import styles from './style.js'
+import ConteudoHeader from "./ConteudoHeader";
+import ConteudoActions from "./ConteudoActions";
+import ConteudoMidia from "./ConteudoMidia";
+import styles from "./style.js";
+import MediaQuery from "react-responsive";
 
 class Conteudo extends React.Component {
   state = { expanded: false };
@@ -23,32 +24,62 @@ class Conteudo extends React.Component {
     const { classes, postagemAtual } = this.props;
 
     return (
-      <Card className={classes.card}>
-        <ConteudoHeader />
-        <CardContent>
-          <Typography component="p" className={classes.estiloTexto}>
-            {postagemAtual.resumo}
-          </Typography>
-        </CardContent>
-        <ConteudoMidia />
-        <ConteudoActions expanded={this.state.expanded} expandirPostagem={() => this.expandirPostagem()} />
-        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            {
-              postagemAtual.tipoMidia === "Texto" &&
-              <Typography paragraph className={classes.estiloTexto}>
-                {postagemAtual.conteudo}
+      <div>
+        <MediaQuery query="(min-device-width: 768px)">
+          <Card className={classes.cardLarge}>
+            <ConteudoHeader />
+            <CardContent>
+              <Typography component="p" className={classes.estiloTexto}>
+                {postagemAtual.resumo}
               </Typography>
-            }
-          </CardContent>
-        </Collapse>
-      </Card>
+            </CardContent>
+            <ConteudoMidia />
+            <ConteudoActions
+              expanded={this.state.expanded}
+              expandirPostagem={() => this.expandirPostagem()}
+            />
+            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                {postagemAtual.tipoMidia === "Texto" && (
+                  <Typography paragraph className={classes.estiloTexto}>
+                    {postagemAtual.conteudo}
+                  </Typography>
+                )}
+              </CardContent>
+            </Collapse>
+          </Card>
+        </MediaQuery>
+        <MediaQuery query="(max-device-width: 768px)">
+          <Card className={classes.cardSmall}>
+            <ConteudoHeader />
+            <CardContent>
+              <Typography component="p" className={classes.estiloTexto}>
+                {postagemAtual.resumo}
+              </Typography>
+            </CardContent>
+            <ConteudoMidia />
+            <ConteudoActions
+              expanded={this.state.expanded}
+              expandirPostagem={() => this.expandirPostagem()}
+            />
+            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                {postagemAtual.tipoMidia === "Texto" && (
+                  <Typography paragraph className={classes.estiloTexto}>
+                    {postagemAtual.conteudo}
+                  </Typography>
+                )}
+              </CardContent>
+            </Collapse>
+          </Card>
+        </MediaQuery>
+      </div>
     );
   }
 }
 
 Conteudo.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 const mapStateToProps = store => {
